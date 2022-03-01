d4cecbd7cf7836475b91e2e83b848bd6accd44be
qiyuan11111 <1746257500@qq.com>
2022-03-01 22:22:54 +0800

    修改目录结构

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo su elasticsearch << EOF
-/usr/local/src/elasticsearch-5.0.1/bin/elasticsearch -d
+/usr/local/src/elasticsearch-6.0.1/bin/elasticsearch -d
 EOF
 sleep 10
 mongo-connector -m Element-mongodb:27017 -t Element-elasticsearch:9200 -d elastic2_doc_manager -a root -p ctguacm1234@
